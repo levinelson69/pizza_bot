c75e48e95051bd73bbece80c651df8f220813ec5
levinelson69 <99291501+levinelson69@users.noreply.github.com>
2022-02-28 10:32:07 +1300

    Pickup Version 4
    Created a function for not_blank and used it to validate name and phone

--- components/pickupv3.py
+++ components/pickupv3.py
@@ -1,9 +1,10 @@
+#Customer details dictonary 
 customer_details = {}
 
-
+#Basic instructions 
 print("Please enter the pickup information")
 
-#Customer name
+#Customer name not blank
 valid = False
 while not valid:
     customer_details['name'] = input("Please enter your name ")
@@ -14,7 +15,7 @@ while not valid:
         print("Sorry this cannot be blank")
 
 
-#Customer phone number
+#Customer phone number not blank
 valid = False
 while not valid:
     customer_details['phone'] = input("Please enter your phone number ")
@@ -23,3 +24,5 @@ while not valid:
         break
     else:
         print("Sorry this cannot be blank")
+
+print(customer_details)
