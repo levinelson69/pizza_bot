519d27a21377bc9700f8437d921db2f6457cb7b0
levinelson69 <99291501+levinelson69@users.noreply.github.com>
2022-03-11 14:45:17 +1300

    Added pizza choice
    Pizza choice system added

--- pizza_bot.py
+++ pizza_bot.py
@@ -98,7 +98,6 @@ def delivery_info():
     question = ("Please enter your suburb ")
     customer_details['suburb'] = not_blank(question )
     print (customer_details['suburb'])
-    print(customer_details)
 
 
 # Pizza menu
